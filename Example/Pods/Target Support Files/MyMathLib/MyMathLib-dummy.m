ca045e40c66a8a5e2122db79ee7e70d401d8664e
Harshitha-CB <harshithaneygar@gmail.com>
2022-06-06 22:21:51 -0700

    updated pod spec

---
+++ Example/Pods/Target Support Files/MyMathLib/MyMathLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyMathLib : NSObject
+@end
+@implementation PodsDummy_MyMathLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyMathLib_Example/Pods-MyMathLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyMathLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyMathLib_Example
+@end
